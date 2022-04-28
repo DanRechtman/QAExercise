fb467617531eb5265b12eaf827f2a38af110a47c
Dan Rechtman <dannyrechtman@gmail.com>
2022-04-27 20:47:26 -0400

    Almost done tests

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 class Data:
     CHROME_EXEC = "C:/Users/minic/Downloads/chromedriver_win32/chromedriver.exe"    
-    
+    FIREFOX="C:/Users/minic/Downloads/geckodriver-v0.31.0-win64/geckodriver.exe"
     BASE_URL = "https://www.realtor.ca/"
     
     
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 
 from __future__ import annotations
+from ast import Suite
 from locators import Locators
 from data import Data, Results
 
@@ -11,8 +12,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-
-
+import unittest
 ## Sources that helped: https://medium.com/@asheeshmisra29/web-automation-selenium-webdriver-and-python-getting-started-part-3-a9c07143d36d 
 
 class BasePage:
@@ -30,6 +30,7 @@ class MainPage(BasePage):
     
     def click_home(self)->MapPage:
         # self.driver.find_element(*Locators.BTN_FindHome).click()
+        self.driver.get(Data.BASE_URL)
         WebDriverWait(self.driver,100).until(EC.visibility_of_element_located(Locators.BTN_FindHome)).click()
         return MapPage(driver=self.driver)
 
@@ -62,19 +63,19 @@ class MapPage(BasePage):
         return self
     
     def select_area(self,keys)->MapPage:
-        WebDriverWait(self.driver,100).until(EC.visibility_of_element_located(Locators.Input_Filter)).send_keys(keys+Keys.RETURN)
+        WebDriverWait(self.driver,100).until(EC.visibility_of_element_located(Locators.Input_Filter)).send_keys(keys)
         return self
         
     def select_date(self)->MapPage:
         sleep(3)
         a= WebDriverWait(self.driver,100).until(EC.visibility_of_element_located(Locators.Filter_Date))
         self.driver.execute_script("arguments[0].removeAttribute('readonly')",a)
-        a.send_keys("01/01/2022"+Keys.RETURN)
-        WebDriverWait(self.driver,100).until(EC.visibility_of_element_located(Locators.BTN_filter_Search)).click()
+        a.send_keys("01/01/2022")
+        # WebDriverWait(self.driver,100).until(EC.visibility_of_element_located(Locators.BTN_filter_Search)).click()
         
         return self
     def select_search(self)->MapPage:
-        WebDriverWait(self.driver,2).until(EC.visibility_of_element_located(Locators.Input_Filter)).click()
+        WebDriverWait(self.driver,100).until(EC.visibility_of_element_located((By.ID,"mapMoreFiltersSearchBtn"))).click()
         # WebDriverWait(self.driver,100).until(EC.visibility_of_element_located(Locators.CLOSE)).click()
         
         return self
@@ -102,49 +103,116 @@ class MapPage(BasePage):
                 self.driver.switch_to.window(w)
         return self
     def getData(self):
-        self.result.price = driver.find_element(By.ID,"listingPrice").get_attribute("innerText")
-        self.result.address = driver.find_element(By.ID,"listingAddress").get_attribute("innerText")
-        self.result.mls = driver.find_element(By.ID,"listingMLSNum").get_attribute("innerText")
-        self.result.summary = driver.find_element(By.ID,"propertyDescriptionCon").get_attribute("innerText")
+        self.result.price = self.driver.find_element(By.ID,"listingPrice").get_attribute("innerText")
+        self.result.address = self.driver.find_element(By.ID,"listingAddress").get_attribute("innerText")
+        self.result.mls = self.driver.find_element(By.ID,"listingMLSNum").get_attribute("innerText")
+        self.result.summary = self.driver.find_element(By.ID,"propertyDescriptionCon").get_attribute("innerText")
 
+
+class Test_Home_Base(unittest.TestCase):
     
+    driver = webdriver.Chrome(executable_path=Data.CHROME_EXEC)
+    
+    def setUp(self) -> None:
+        # service = ChromiumService(Data.CHROME_EXEC,start_error_message="Error")
+        # self.driver = webdriver.Chrome(service=service)
+        pass
+        
+    def tearDown(self) -> None:
+        # self.driver.close()
+        # self.driver.quit()
+        pass
+
+class Test_Home(Test_Home_Base):
+    cPAGE:MapPage = None
+    def setUp(self):
+        return super().setUp()
+    def test_01_home_page(self):
+
+        m= MainPage(driver=self.driver).click_home()
+        self.cPAGE = m
+        self.assertIn("https://www.realtor.ca/map",m.driver.current_url)
+        # m.click_filter()
+        # self.assertEqual("","")
+        
+    def test_02_filter(self):
+        
+        m= MapPage(driver=self.driver).click_filter()
+        box = self.driver.find_element(By.ID,"mapSearchMoreCon")
+        self.assertEqual(True,box.is_displayed())
+    def test_03_res(self):
+        MapPage(driver=self.driver).select_residential()
+        box = self.driver.find_element(By.ID,"select2-ddlPropertyTypeRes-container")
+        self.assertEqual("Residential",box.text)
+    def test_04_town(self):
+        MapPage(driver=self.driver).select_townhouse()
+        box = self.driver.find_element(By.ID,"select2-ddlBuildingType-container").text
+        self.assertEqual("Row / Townhouse",box)
+        
+
+    def test_05_bed_bath(self):
+        MapPage(driver=self.driver).select_bed().select_bath()
+        bed = self.driver.find_element(*Locators.BTN_Dropdown_Bed_Num).text
+        bath = self.driver.find_element(*Locators.BTN_Dropdown_Bath_Num).text
+        self.assertEqual("3",bed) and self.assertEqual("3",bath)
+    def test_06_area(self):
+        MapPage(driver=self.driver).select_area("Richmond Hill, Ontario")
+        box = self.driver.find_element(By.ID,"mapMoreSearchTxt")
+        value = box.get_attribute("value")
+        self.assertEqual("Richmond Hill, Ontario",value)
+    
+    def test_07_date(self):
+        MapPage(driver=self.driver).select_date()
+        box = self.driver.find_element(*Locators.Filter_Date)
+        value = box.get_attribute("value")
+        self.assertEqual("01/01/2022",value)
+    def test_08_Search(self):
+        MapPage(driver=self.driver).select_search()
+        sleep(1)
+        box = self.driver.find_element(By.ID,"mapSearchMoreCon")
+        self.assertEqual(False,box.is_displayed())
+
 if __name__ =="__main__":
     
-    service = ChromiumService(Data.CHROME_EXEC,start_error_message="Error")
-    driver = webdriver.Chrome(service=service)
+    # service = ChromiumService(Data.CHROME_EXEC,start_error_message="Error")
+    # driver = webdriver.Chrome(service=service)
 
-    driver.get(Data.BASE_URL)
+    # driver.get(Data.BASE_URL)
     
-    mapPage = MainPage(driver=driver).click_home()
-    mapPage.click_filter()\
-           .select_residential()\
-           .select_townhouse()\
-           .select_bed()\
-           .select_bath()\
-           .select_area("Richmond Hill, Ontario")\
-           .click_filter()\
-           .select_date()\
-           .getTotalResult()\
-           .sortResult()\
-           .clickCard()\
-           .getData()
-    driver.get(Data.BASE_URL)
-    mapPage = MainPage(driver=driver).click_home()
-    mapPage.click_filter()\
-           .select_residential()\
-           .select_townhouse()\
-           .select_bed()\
-           .select_bath()\
-           .select_area("Scarborough, Ontario")\
-           .click_filter()\
-           .select_date()\
-           .getTotalResult()\
-           .sortResult()\
-           .clickCard()\
-           .getData()
-    print(mapPage.result)
-    sleep(100)
+    # mapPage = MainPage(driver=driver).click_home()
+    # mapPage.click_filter()\
+    #        .select_residential()\
+    #        .select_townhouse()\
+    #        .select_bed()\
+    #        .select_bath()\
+    #        .select_area("Richmond Hill, Ontario")\
+    #        .click_filter()\
+    #        .select_date()\
+    #        .getTotalResult()\
+    #        .sortResult()\
+    #        .clickCard()\
+    #        .getData()
+    # driver.get(Data.BASE_URL)
+    # mapPage = MainPage(driver=driver).click_home()
+    # mapPage.click_filter()\
+    #        .select_residential()\
+    #        .select_townhouse()\
+    #        .select_bed()\
+    #        .select_bath()\
+    #        .select_area("Scarborough, Ontario")\
+    #        .click_filter()\
+    #        .select_date()\
+    #        .getTotalResult()\
+    #        .sortResult()\
+    #        .clickCard()\
+    #        .getData()
+    # print(mapPage.result)
+    
+    # Test_Home().test_home_page()
+    # runner = unittest.TextTestRunner(failfast=True)
+    # runner.run(suite())
+    unittest.main()
+    # sleep(1)
                           
 
-    
     
