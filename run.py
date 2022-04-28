3ddce8bf403b2c8c9438298d68349778b4ab2d70
Dan Rechtman <dannyrechtman@gmail.com>
2022-04-28 17:21:12 -0400

    finished all tests

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
-
 from __future__ import annotations
-from ast import Suite
-from locators import Locators
-from data import Data, Results
+
+from time import sleep
+import unittest
+from datetime import datetime
 
 from selenium.webdriver.common.by import By
-
 from selenium.webdriver.chromium.service import ChromiumService
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from time import sleep
-import unittest
-## Sources that helped: https://medium.com/@asheeshmisra29/web-automation-selenium-webdriver-and-python-getting-started-part-3-a9c07143d36d 
+import HtmlTestRunner
+
+
+from locators import Locators
+from data import Data, Results
+
+
+# Sources that helped: https://medium.com/@asheeshmisra29/web-automation-selenium-webdriver-and-python-getting-started-part-3-a9c07143d36d
 
 class BasePage:
     """Basepage, class where all other pages extend from"""
@@ -21,14 +25,21 @@ class BasePage:
         """__init__
 
         Args:
-            driver (WebDriver): WebDriver
+            driver (webdriver): webdriver
         """
         self.driver = driver
-        
-        
 class MainPage(BasePage):
-    
+    """MainPage
+
+    Args:
+        BasePage (webdriver): webdriver
+    """
     def click_home(self)->MapPage:
+        """click_home
+
+        Returns:
+            MapPage: this
+        """
         # self.driver.find_element(*Locators.BTN_FindHome).click()
         self.driver.get(Data.BASE_URL)
         WebDriverWait(self.driver,100).until(EC.visibility_of_element_located(Locators.BTN_FindHome)).click()
@@ -36,9 +47,15 @@ class MainPage(BasePage):
 
 
 class MapPage(BasePage):
+    """MapPage /map of realtor.ca
+
+    Args:
+        BasePage (webdriver): webdriver
+
+    """
     result = Results()
 
-    def click_filter(self)->MapPage:
+    def click_filter(self)->MapPage:       
         WebDriverWait(self.driver,100).until(EC.visibility_of_element_located(Locators.BTN_filter)).click()
         return self
     
@@ -107,7 +124,7 @@ class MapPage(BasePage):
         self.result.address = self.driver.find_element(By.ID,"listingAddress").get_attribute("innerText")
         self.result.mls = self.driver.find_element(By.ID,"listingMLSNum").get_attribute("innerText")
         self.result.summary = self.driver.find_element(By.ID,"propertyDescriptionCon").get_attribute("innerText")
-
+        return self
 
 class Test_Home_Base(unittest.TestCase):
     
@@ -116,17 +133,20 @@ class Test_Home_Base(unittest.TestCase):
     def setUp(self) -> None:
         # service = ChromiumService(Data.CHROME_EXEC,start_error_message="Error")
         # self.driver = webdriver.Chrome(service=service)
-        pass
+        self.start = datetime.now()
+        
         
     def tearDown(self) -> None:
         # self.driver.close()
         # self.driver.quit()
-        pass
+        self.end = datetime.now()
+        diff = self.end - self.start
+        print(diff.microseconds/1000,"ms")
 
 class Test_Home(Test_Home_Base):
     cPAGE:MapPage = None
-    def setUp(self):
-        return super().setUp()
+    # def setUp(self):
+    #     return super().setUp()
     def test_01_home_page(self):
 
         m= MainPage(driver=self.driver).click_home()
@@ -137,9 +157,9 @@ class Test_Home(Test_Home_Base):
         
     def test_02_filter(self):
         
-        m= MapPage(driver=self.driver).click_filter()
+        MapPage(driver=self.driver).click_filter()
         box = self.driver.find_element(By.ID,"mapSearchMoreCon")
-        self.assertEqual(True,box.is_displayed())
+        self.assertTrue(box.is_displayed())
     def test_03_res(self):
         MapPage(driver=self.driver).select_residential()
         box = self.driver.find_element(By.ID,"select2-ddlPropertyTypeRes-container")
@@ -154,7 +174,9 @@ class Test_Home(Test_Home_Base):
         MapPage(driver=self.driver).select_bed().select_bath()
         bed = self.driver.find_element(*Locators.BTN_Dropdown_Bed_Num).text
         bath = self.driver.find_element(*Locators.BTN_Dropdown_Bath_Num).text
-        self.assertEqual("3",bed) and self.assertEqual("3",bath)
+        self.assertEqual("3",bed)
+        self.assertEqual("3",bath)
+        
     def test_06_area(self):
         MapPage(driver=self.driver).select_area("Richmond Hill, Ontario")
         box = self.driver.find_element(By.ID,"mapMoreSearchTxt")
@@ -171,14 +193,24 @@ class Test_Home(Test_Home_Base):
         sleep(1)
         box = self.driver.find_element(By.ID,"mapSearchMoreCon")
         self.assertEqual(False,box.is_displayed())
-
+    def test_09_results(self):
+        result = int(MapPage(driver=self.driver).getTotalResult().result.num)
+        self.assertIsInstance(result,int)
+        print(f"{result=}")
+        
+    def test_10_info(self):
+        House = MapPage(driver=self.driver).sortResult().clickCard().getData().result
+        self.assertIsInstance(House,Results)
+        
+        print(f"{House.price=}")
+        print(f"{House.address=}")
+        print(f"{House.mls=}")
+        print(f"{House.summary=}")
+          
 if __name__ =="__main__":
-    
     # service = ChromiumService(Data.CHROME_EXEC,start_error_message="Error")
     # driver = webdriver.Chrome(service=service)
-
     # driver.get(Data.BASE_URL)
-    
     # mapPage = MainPage(driver=driver).click_home()
     # mapPage.click_filter()\
     #        .select_residential()\
@@ -207,11 +239,13 @@ if __name__ =="__main__":
     #        .clickCard()\
     #        .getData()
     # print(mapPage.result)
-    
     # Test_Home().test_home_page()
     # runner = unittest.TextTestRunner(failfast=True)
     # runner.run(suite())
-    unittest.main()
+    # template_args={
+    #     "time" : 
+    # }
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(add_timestamp=True))
     # sleep(1)
                           
 
